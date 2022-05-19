4d2e893194b31d4845e30174b681939597534629
Viegas <mvrdgs@gmail.com>
2022-05-19 12:05:07 -0300

    exercicio 3

---
+++ dia1/exercicio3.go
@@ -0,0 +1,10 @@
+package dia1
+
+func correcaoVariaveis() {
+	// var nome1 string
+	// var sobrenome string
+	// var idade int
+	// var licencaParaDirigir bool = true
+	// var estaturaDaPessoa int
+	// quantidadeDeFilhos := 2
+}
