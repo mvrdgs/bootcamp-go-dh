6177d9df19584383b8735166788f03cd05106cc6
Viegas <mvrdgs@gmail.com>
2022-05-20 11:58:29 -0300

    correcao exercicio 1

--- dia2/manha1.go
+++ dia2/manha1.go
@@ -1,10 +1,10 @@
 package dia2
 
 func SalaryTax(salary float64) (tax float64) {
+	tax = salary * .17
 	if salary > 50000 {
-		tax = salary * .17
-		return tax
+		tax += salary * .10
 	}
-	tax = salary * .10
+
 	return tax
 }
