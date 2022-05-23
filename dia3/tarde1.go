d93eeb2ab43a0c0ffab6a189f8f0a9554a75b490
Viegas <mvrdgs@gmail.com>
2022-05-23 17:06:28 -0300

    exercicio 1 dia 3 tarde

---
+++ dia3/tarde1.go
@@ -0,0 +1,27 @@
+package dia3
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// Uma empresa que vende produtos de limpeza precisa de:
+// 1. Implementar uma funcionalidade para guardar um arquivo de texto, com a informação
+// de produtos comprados, separados por ponto e vírgula (csv).
+// 2. Deve possuir o ID do produto, preço e a quantidade.
+// 3. Estes valores podem ser hardcodeados ou escritos manualmente em uma variável.
+
+func SaveProduct(id, qtd int, price float64) {
+	info := fmt.Sprintf("%d - %d - %.2f;\n", id, qtd, price)
+
+	f, err := os.OpenFile("./ProductInfo.txt",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(string(info)); err != nil {
+		log.Println(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/mvrdgs/bootcamp-go-dh/dia2"
+	"github.com/mvrdgs/bootcamp-go-dh/dia3"
 )
 
 func main() {
-	loja := dia2.NovaLoja()
-
-	mouse := dia2.NovoProduto("Mouse", "Pequeno", 100.0)
-	macBook := dia2.NovoProduto("MacBook Pro", "Medio", 20000.00)
-	frigobar := dia2.NovoProduto("Frigobar", "Grande", 3000.00)
-
-	fmt.Println("Custo mouse: ", mouse.CalcularCusto())
-	fmt.Println("Custo Mac Book: ", macBook.CalcularCusto())
-	fmt.Println("Custo Frigobar: ", frigobar.CalcularCusto())
-
-	loja.Adicionar(mouse)
-	loja.Adicionar(macBook)
-	loja.Adicionar(frigobar)
-
-	fmt.Println("Total: ", loja.Total())
+	dia3.SaveProduct(1, 10, 36.35)
 }
