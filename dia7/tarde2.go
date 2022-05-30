7d2740e230f34228ad5a9d6728ce5436e4dc1242
Mauricio Viegas Rodrigues <mviegas@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-05-30 15:04:24 -0300

    dia 3 exercicio 3 tarde

--- dia7/tarde2.go
+++ dia7/tarde2.go
@@ -1,9 +1,42 @@
 package dia7
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
 )
+
+//type Products struct {
+//	Products []Product `json:"products"`
+//}
+
+type Product struct {
+	Id            int     `json:"id,omitempty"`
+	Nome          string  `json:"nome,omitempty"`
+	Cor           string  `json:"cor,omitempty"`
+	Preco         float64 `json:"preco,omitempty"`
+	Estoque       int     `json:"estoque,omitempty"`
+	Codigo        string  `json:"codigo,omitempty"`
+	Publicacao    bool    `json:"publicacao,omitempty"`
+	DataDeCriacao string  `json:"data_de_criacao,omitempty"`
+}
+
+func ReadFile() []Product {
+	file, _ := ioutil.ReadFile("./dia7/products.json")
+
+	data := make([]Product, 0)
+
+	fmt.Println(string(file))
+
+	err := json.Unmarshal([]byte(file), &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return data
+}
 
 func Tarde2() {
 	router := gin.Default()
@@ -12,6 +45,12 @@ func Tarde2() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello Maurício",
 		})
+	})
+
+	data := ReadFile()
+
+	router.GET("/products", func(c *gin.Context) {
+		c.JSON(http.StatusOK, data)
 	})
 
 	router.Run()
