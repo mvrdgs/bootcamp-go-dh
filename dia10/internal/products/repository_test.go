b18d93b4f250ab2a2e7f7b1c1b55b5801ebc4108
Mauricio Viegas <mvrdgs@gmail.com>
2022-06-08 12:51:24 -0300

    corrigido exercicio 1, feito exercicio 2

--- dia10/internal/products/repository_test.go
+++ dia10/internal/products/repository_test.go
@@ -1,23 +1,27 @@
 package products
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 type Store struct {
 	products []Product
+	updated  bool
 }
 
-func (s Store) Read(data interface{}) error {
+func (s *Store) Read(data interface{}) error {
+	s.updated = true
+	products, _ := json.Marshal(s.products)
+	return json.Unmarshal(products, &data)
+}
+
+func (s *Store) Write(data interface{}) error {
 	return nil
 }
 
-func (s Store) Write(data interface{}) error {
-	return nil
-}
-
-func (r repository) GetAllTest(t *testing.T) {
+func TestRepository_GetAll(t *testing.T) {
 	products := []Product{
 		{
 			ID:    1,
@@ -35,8 +39,24 @@ func (r repository) GetAllTest(t *testing.T) {
 		},
 	}
 	db := Store{products: products}
-	repo := NewRepository(db)
+	repo := NewRepository(&db)
 	result, _ := repo.GetAll()
 
-	assert.Equal(t, result, products)
+	assert.Equal(t, products, result)
+}
+
+func TestRepository_UpdateName(t *testing.T) {
+	products := []Product{{
+		ID:    1,
+		Name:  "test",
+		Type:  "test",
+		Count: 1,
+		Price: 1,
+	}}
+	db := Store{products: products, updated: false}
+	repo := NewRepository(&db)
+
+	repo.UpdateName(1, "atualizado")
+
+	assert.True(t, db.updated)
 }
