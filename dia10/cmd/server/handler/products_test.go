45456b33b1ddf7e48d4837d0c5f7b2dee4e0f1d2
Mauricio Viegas <mvrdgs@gmail.com>
2022-06-10 15:01:07 -0300

    adicionado teste para rota post

--- dia10/cmd/server/handler/products_test.go
+++ dia10/cmd/server/handler/products_test.go
@@ -2,9 +2,9 @@ package handler
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/mvrdgs/bootcamp-go-dh/dia10/internal/products"
-	"github.com/mvrdgs/bootcamp-go-dh/dia10/pkg/store"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -12,11 +12,41 @@ import (
 	"testing"
 )
 
+type Store struct {
+	products []Product
+	updated  bool
+	err      error
+}
+
+func (s *Store) Read(data interface{}) error {
+	s.updated = true
+	prods, _ := json.Marshal(s.products)
+	if s.err != nil {
+		return s.err
+	}
+
+	return json.Unmarshal(prods, &data)
+}
+
+func (s *Store) Write(data interface{}) error {
+	prod, _ := json.Marshal(data)
+	err := json.Unmarshal(prod, &s.products)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func createServer() *gin.Engine {
 	_ = os.Setenv("TOKEN", "123456")
 
-	db := store.New(store.FileType, "./products_test.json")
-	repo := products.NewRepository(db)
+	db := Store{
+		products: []Product{},
+		updated:  false,
+		err:      nil,
+	}
+	repo := products.NewRepository(&db)
 	service := products.NewService(repo)
 	p := NewProduct(service)
 	r := gin.Default()
@@ -40,6 +70,16 @@ func TestProduct_GetAll(t *testing.T) {
 	req, rr := createRequestTest(http.MethodGet, "/products", "")
 
 	r.ServeHTTP(rr, req)
-	
+
 	assert.Equal(t, 200, rr.Code)
+}
+
+func TestProduct_Store(t *testing.T) {
+	r := createServer()
+	body := `{"name": "test", "type": "ola", "count": 10, "price": 10.99}`
+	req, rr := createRequestTest(http.MethodPost, "/products", body)
+
+	r.ServeHTTP(rr, req)
+	
+	assert.Equal(t, 201, rr.Code)
 }
